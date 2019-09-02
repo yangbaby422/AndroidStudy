066da6ce9c94cc5346875817a15f232c7d9a14b3
yangchen <543748493@qq.com>
2019-09-02 23:57:15 +0800

    second commit

---
+++ app/src/main/java/com/example/helloworld/RegisterActivity.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+}
